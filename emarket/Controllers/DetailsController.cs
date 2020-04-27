cc39503c22ab305909077bbe2d745b842441645b
rewas <rewas@REWAS>
2020-04-27 23:41:03 +0200

    Add project files.

---
+++ emarket/Controllers/DetailsController.cs
@@ -0,0 +1,139 @@
+﻿using emarket.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace emarket.Controllers
+{
+
+    public class DetailsController : Controller
+    {
+        private StoreEntities db = new StoreEntities();
+        // GET: Details
+        public ActionResult Details(int id)
+        {
+            var product = db.Products.Find(id);
+            if (product != null)
+            {
+                ViewBag.Product = product;
+                return View();
+            }
+            else
+            {
+                return HttpNotFound();
+            }
+        }
+
+        //<------------------------Update Product---------------------->//
+        //HttpGet for Update:-----------------
+        [HttpGet]
+        public ActionResult Update(int id)
+        {
+            var product = db.Products.SingleOrDefault(m => m.id == id);
+            if (product != null)
+            {
+
+                ViewBag.Product = product;
+                ViewBag.Category = db.Categories.ToList();
+                return View();
+            }
+            else
+            {
+                return HttpNotFound();
+            }
+        }
+        //HttpPost for Update:-------------
+        [HttpPost]
+        public object Update(Product product, HttpPostedFileBase up)
+        {
+            var update = db.Products.SingleOrDefault(p => p.id == product.id);
+            if (ModelState.IsValid)
+            {
+
+                update.name = product.name;
+                update.price = product.price;
+                update.description = product.description;
+                update.category_id = product.category_id;
+                if (up != null)
+                {
+                    String path = Path.Combine(Server.MapPath("~/Uploads"), up.FileName);
+                    up.SaveAs(path);
+                    update.image = up.FileName;
+
+                }
+                
+                db.Entry(update).State = EntityState.Modified;
+                db.SaveChanges();
+               
+                return RedirectToAction("Details/" + product.id);
+            }
+            else
+            {
+                return HttpNotFound();
+            }
+            
+            
+
+        }
+
+
+
+        //<----------------------Delect Product------------------>//
+        //HttpGet for Delete:-----------
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            var product = db.Products.Find(id);
+            
+           
+            if (product == null)
+            {
+                return RedirectToAction("Index","Home");
+            }
+            else
+            {
+               
+                ViewBag.Product = product;
+                return View();
+            }
+        }
+        //HttpPost for Delete:--------------
+        [HttpPost, ActionName("Delete")]
+        public ActionResult Delete(Product product)
+        {
+            var remove = db.Products.Find(product.id);
+
+            if (remove == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                if (ModelState.IsValid)
+                {
+                    var category = db.Categories.SingleOrDefault(c => c.id == remove.category_id);
+                    if (category.number_of_products > 0)
+                    {
+                        category.number_of_products--;
+
+                        db.Entry(category).State = EntityState.Modified;
+                    }
+
+
+                    db.Products.Remove(remove);
+
+                    db.SaveChanges();
+                }
+            }
+            return RedirectToAction("Delete");
+        }
+        //=======================
+
+
+
+    }
+}
---
+++ emarket/Controllers/HomeController.cs
@@ -0,0 +1,93 @@
+﻿using emarket.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace emarket.Controllers
+{
+    public class HomeController : Controller
+    {
+        private StoreEntities db = new StoreEntities();
+        public ActionResult Index()
+        {
+            ViewBag.Product =db.Products.ToList();
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(String searchCategory)
+        {
+
+            if (String.IsNullOrEmpty(searchCategory))
+            {
+                ViewBag.Product = db.Products.ToList();
+            }
+            else
+            {
+
+                var product = db.Products.Where(p => p.Category.name.Contains(searchCategory)).ToList();
+                
+                if (product.Count == 0 || product.Count == null)
+                {
+                    ViewBag.message = "No Result";
+                }
+
+                ViewBag.Product = product;
+
+            }
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+            return View();
+        }
+
+
+        //<------------------------Add Product---------------------->//
+        //HttpGet for Update:-----------------
+        [HttpGet]
+        public ActionResult Add()
+        {
+            ViewBag.Category = db.Categories.ToList();
+            return View();
+        }
+        //HttpPost for Update:-----------------
+        [HttpPost]
+        public object Add(Product product, HttpPostedFileBase upload)
+        {
+           
+            
+            if (ModelState.IsValid)
+            {
+                String path = Path.Combine(Server.MapPath("~/Uploads"), upload.FileName);
+                upload.SaveAs(path);
+                product.image = upload.FileName;
+               
+                db.Products.Add(product);
+                db.SaveChanges();
+                var add = db.Categories.SingleOrDefault(m => m.id == product.category_id);
+                add.number_of_products++;
+                db.Entry(add).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+
+            }
+
+
+            return RedirectToAction("Index");
+        }
+        //=========================================
+
+
+    }
+}
